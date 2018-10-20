1dd4cfefb7f111b3874ed9a1c6bd7a3807d72134
Ted Pyne <ted.pyne.4@gmail.com>
2018-10-19 22:03:36 -0400

    Clean data and better parser

--- cleanmonth.py
+++ cleanmonth.py
@@ -6,17 +6,17 @@ with open(sys.argv[1]) as csv_file:
     date = sys.argv[1].split('-')
     accept_count = 0
     reject_count = 0
+    spec_hit = 0
     outcsv = open(date[0]+'_out.csv', 'w')
     for row in csv_reader:
-        if(row[13]=='1969' or row[14]=='0'):
+        if(row[13]=='1969' or row[14]=='0' or row[0]=='tripduration'):
             reject_count+=1
         else:
+            if(len(sys.argv)>3 and row[3]==sys.argv[2] and row[7]==sys.argv[3]):
+                spec_hit+=1
+                print(row[1] + " " + row[2] + " "+ row[7])
             accept_count+=1
-            outcsv.write(row[0]+","+row[1]+","+row[3]+","+row[7]+","+row[12]+","+row[13]+","+row[14]+"\n")
-        '''if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1'''
-print(f'Pass {accept_count}\nFail {reject_count}')
+            dt = row[1].split(" ")
+            outcsv.write(row[0]+","+dt[0]+","+dt[1]+","+row[3]+","+row[7]+","+row[12]+","+row[13]+","+row[14]+"\n")
+
+print(f'Pass {accept_count}\nFail {reject_count}\nSpec {spec_hit}')
