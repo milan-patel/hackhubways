ab47b508d38354af30e1376747cf2576e04ca674
Ted Pyne <ted.pyne.4@gmail.com>
2018-10-19 23:59:25 -0400

    Cleaned up program

--- reprt.py
+++ reprt.py
@@ -9,12 +9,11 @@ with open("rt.csv") as csv_file:
         sig = row[3]+","+row[4]+","+row[5]+","+row[6]
         if(sig in multicol):
             multicol[sig]+=1
-            print("HIT")
             hit+=1
         else:
             multicol[sig]=1
 for k in list(multicol.keys()):
-    if(multicol[k]<10):
+    if(multicol[k]<50):
         del multicol[k];
 
 print(multicol)
