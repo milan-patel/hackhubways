ce1906cf0be649111e3449497ebe5d0a76e1dac7
Ted Pyne <ted.pyne.4@gmail.com>
2018-10-19 23:58:25 -0400

    Multicol

---
+++ reprt.py
@@ -0,0 +1,21 @@
+import csv
+import os
+
+multicol = {}
+hit=0
+with open("rt.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        sig = row[3]+","+row[4]+","+row[5]+","+row[6]
+        if(sig in multicol):
+            multicol[sig]+=1
+            print("HIT")
+            hit+=1
+        else:
+            multicol[sig]=1
+for k in list(multicol.keys()):
+    if(multicol[k]<10):
+        del multicol[k];
+
+print(multicol)
+print(len(multicol))
