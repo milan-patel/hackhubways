76137f830e56b6391f7abc5a6bf3dd46cfb47370
milan-patel <milan@Milans-MacBook-Air.local>
2018-10-20 21:28:49 -0400

    ran the algorithm a few times to generate sample data

--- identifier.py
+++ identifier.py
@@ -11,6 +11,15 @@ from zipCodeConverter import *
 from datetime import datetime
 from datetime import timedelta
 
+import timeit
+
+start = timeit.default_timer()
+
+def convertStrToDateTime(s):
+    s = datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
+    return s
+    
+
 # search through all time stamps for 35-54yo females biking between two stations
 # on weekdays, leaving between 7-10AM and departing 6-10 hours after their arrival
 # over the last 3 months
@@ -19,10 +28,11 @@ header = ['tripduration', 'starttime', 'stoptime', 'start station id', 'start st
 
 csv_files = ["rawdata/201807-bluebikes-tripdata.csv","rawdata/201808-bluebikes-tripdata.csv","rawdata/201809-bluebikes-tripdata.csv"]
 
-# 8 possible stations within the South Boston Waterfront Area
-stations = stationsByZipCode("02210")
+# Possible stations within the South Boston Waterfront Area Zip code
+zipCode = "02210"
+demo = [1983,1964,1] # 
 
-def findTrips(demo, startID, csv_file_names):
+def findTrips(demo, zipCode, csv_file_names):
            # demo is of the form List(Int, Int, Int)
            # where the first int is the max birthyear, 
            # the second is the min birthyear, 
@@ -36,50 +46,65 @@ def findTrips(demo, startID, csv_file_names):
     departures = []
     arrivals = []
     output = [departures, arrivals]
+    stations = stationsByZipCode(zipCode)
     for csv_file_name in csv_file_names: 
         with open(csv_file_name, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             next(csv_reader) # skips the header 
             for row in csv_reader:
-                if(demo[0] > int(row[13]) and int(row[13]) < demo[1] and int(row[14]) == demo[2] and \
-                   startID == row[3]):
-                    row[1] = datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S.%f")
-                    row[2] = datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S.%f")
-                    departures.append(row)
-                elif(demo[0] > int(row[13]) and int(row[13]) < demo[1] and int(row[14]) == demo[2] and \
-                     startID == row[7]):
-                    row[1] = datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S.%f")
-                    row[2] = datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S.%f")
-                    arrivals.append(row)
+                if(row[3] in stations or row[7] in stations):
+                    if(row[3] in stations and demo[0] > int(row[13]) and int(row[13]) > demo[1] and int(row[14]) == demo[2]):
+                        row[1] = convertStrToDateTime(row[1])
+                        row[2] = convertStrToDateTime(row[2])
+                        departures.append(row)
+                    elif(demo[0] > int(row[13]) and int(row[13]) > demo[1] and int(row[14]) == demo[2]):
+                        row[1] = convertStrToDateTime(row[1])
+                        row[2] = convertStrToDateTime(row[2])
+                        arrivals.append(row)
     return output
 
 
 def groupTrips(lst):
-    uppertime = timedelta(hours =10)
+    uppertime = timedelta(hours=10)
     lowertime = timedelta(hours=6)
     
     # takes in the output of findTrips, produces a dictionary of individuals
     # taking trips to and from particular locations frequently
     departures = lst[0]
     arrivals = lst[1] 
-    destinations = {}
-    i = 0 
-    
+    destinations = []
     for departure in departures:
-        i+=1
         end = departure[7]
+        start = departure[3]
         for arrival in arrivals:
-            if(end == arrival[3] and departure[13] == arrival[13] and departure[1].date() == arrival[1].date()):
-                destinations[i] = [departure,arrival]
+            if(departure[1].date() == arrival[1].date() and end == arrival[3] and departure[13] == arrival[13] and \
+               start == arrival[7] and departure[1] + lowertime < arrival[1] and departure[1] + uppertime > arrival[1] ):
+                destinations.append(departure)
+                destinations.append(arrival)
                 break
     return destinations
 
-with open("SampleIndData.csv", 'w') as out:
-    csv_writer = csv.writer(out)
-    csv_writer.writerow(header)
-    for k in range(0,8):
-        for value in (groupTrips(findTrips([1983, 1964,2], stations[k], csv_files)).items()):
-            csv_writer.writerow(value[1][0])
-            csv_writer.writerow(value[1][1])
+def writeIndDataToCSV():
+    title = "Biking Commute Data for "
+    if(demo[2] == 2):
+        title += "Females "
+    else:
+        title += "Males "
+    title+= "Aged "
+    title+= str(2018-demo[0])+" to "+str(2018-demo[1])+" in "+zipCode+".csv"
+    entries = groupTrips(findTrips(demo,zipCode,csv_files))
+    with open(title, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(header)
+        for row in entries:
+            writer.writerow(row)
+    return
+
+writeIndDataToCSV()
+
+# Times the algorithm. Generally want it to complete in under 5 seconds            
+stop = timeit.default_timer()
+            
+print('Time: ', stop - start)  
 
 
